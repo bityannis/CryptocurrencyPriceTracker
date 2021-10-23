5698e6baad96bc1e1f958a5dd22b667a722c5ac9
Yannis <kessas.belhadj@gmail.com>
2021-10-23 12:42:50 +0200

    first commit

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,14 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+String currencyName = 'AUD';
+String cryptoName = 'BTC';
+String url = 'https://rest.coinapi.io/v1/exchangerate/';
+String apiKey = '?apikey=91D7611C-F1F5-4874-B7D5-0E775A0513F5';
+String apiKey2 = '?apikey=CBDA7723-54F6-498E-BD86-401C87AF7581';
+String apiKey3 = '?apikey=E674CD42-E1F1-4D2A-9373-0E1DA6045A41';
+String apiKey4 = '?apiKey=9A8D707D-6414-4152-A624-5032B0A28874';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +39,32 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class GetAPI {
+  GetAPI(this.url);
+
+  final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
+
+class CoinPrice {
+  Future<dynamic> getCryptoPriceInAnyFiat(
+      String cryptoName, String fiat) async {
+    GetAPI coinData = GetAPI('$url$cryptoName/$fiat$apiKey');
+    var jsonCoinPrice = await coinData.getData();
+    if (jsonCoinPrice != null) {
+      return jsonCoinPrice;
+    } else {
+      return;
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io'
+    show
+        Platform; // Take only the Platform class from SDK to check which platform the device is running on.
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,11 +11,114 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String bitcoinPrice = '0.0';
+  String etherPrice = '0.0';
+  String litcoinPrice = '0.0';
+
+  androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItems.add(newItem);
+    }
+
+    return DropdownButton<String>(
+      value: currencyName,
+      items: dropdownItems,
+      onChanged: (newValue) {
+        setState(() {
+          currencyName = newValue;
+        });
+      },
+    );
+  }
+
+  void getCryptoPriceInAnyFiat() async {
+    CoinPrice bitcoinInFiat = CoinPrice();
+    var jsonFile =
+        await bitcoinInFiat.getCryptoPriceInAnyFiat(cryptoName, currencyName);
+    print(jsonFile);
+    var price = jsonFile['rate'].toStringAsFixed(0).toString();
+    print(price);
+
+    var jsonFileEth =
+        await bitcoinInFiat.getCryptoPriceInAnyFiat('ETH', currencyName);
+    print(jsonFileEth);
+    var priceEth = jsonFileEth['rate'].toStringAsFixed(0).toString();
+    print(priceEth);
+
+    var jsonFileLtc =
+        await bitcoinInFiat.getCryptoPriceInAnyFiat('LTC', currencyName);
+    print(jsonFileLtc);
+    var priceLtc = jsonFileLtc['rate'].toStringAsFixed(0).toString();
+    print(priceLtc);
+
+    setState(() {
+      if (jsonFile != null) {
+        bitcoinPrice = price;
+      } else {
+        bitcoinPrice = '?';
+      }
+    });
+
+    setState(() {
+      if (jsonFileEth != null) {
+        etherPrice = priceEth;
+      } else {
+        etherPrice = '?';
+      }
+    });
+
+    setState(() {
+      if (jsonFileLtc != null) {
+        litcoinPrice = priceLtc;
+      } else {
+        litcoinPrice = '?';
+      }
+    });
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> currencies = [];
+
+    for (String currency in currenciesList) {
+      currencies.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      onSelectedItemChanged: (index) {
+        print(index);
+        print(currenciesList[index]);
+        setState(() {
+          currencyName = currenciesList[index];
+        });
+        getCryptoPriceInAnyFiat();
+      },
+      children: currencies,
+    );
+  }
+
   @override
+  //Lance la requete API des le debut pour recuper le prix
+  void initState() {
+    super.initState();
+    // getBitcoinPrice();
+    getCryptoPriceInAnyFiat();
+    print(currencyName);
+    print('$url$currencyName$apiKey4');
+  }
+
   Widget build(BuildContext context) {
+    print(bitcoinPrice);
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        title: Text('Crypto Price Tracker 💸'),
+        backgroundColor: Colors.blueGrey,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -19,7 +127,7 @@ class _PriceScreenState extends State<PriceScreen> {
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
-              color: Colors.lightBlueAccent,
+              color: Colors.orange,
               elevation: 5.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
@@ -27,7 +135,49 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoinPrice $currencyName',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.teal,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 ETH = $etherPrice $currencyName',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.grey,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 LTC = $litcoinPrice $currencyName',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -41,8 +191,8 @@ class _PriceScreenState extends State<PriceScreen> {
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
+            color: Colors.blueGrey[400],
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
